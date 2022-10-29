c026067576637dd58f1b456e36cee699044fac0a
Knightmare5280 <83542455+Knightmare5280@users.noreply.github.com>
2022-10-28 23:25:52 -0600

    test 1.2

--- src/drivers/drv_spi_gyro.c
+++ src/drivers/drv_spi_gyro.c
@@ -31,10 +31,6 @@ static gyro_types_t gyro_spi_detect() {
     // FALLTHROUGH
 
   case GYRO_TYPE_ICM42605:
-<<<<<<< HEAD
-  case GYRO_TYPE_ICM42688P:
-=======
->>>>>>> 53276eff (auto detect gyro type)
     type = icm42605_detect();
     if (type != GYRO_TYPE_INVALID) {
       break;
@@ -101,55 +97,6 @@ gyro_data_t gyro_spi_read() {
   case GYRO_TYPE_ICM20689: {
     uint8_t buf[14];
     mpu6xxx_read_data(MPU_RA_ACCEL_XOUT_H, buf, 14);
-<<<<<<< HEAD
-
-    data.accel.axis[0] = -(int16_t)((buf[0] << 8) | buf[1]);
-    data.accel.axis[1] = -(int16_t)((buf[2] << 8) | buf[3]);
-    data.accel.axis[2] = (int16_t)((buf[4] << 8) | buf[5]);
-
-    data.temp = (float)((int16_t)((buf[6] << 8) | buf[7])) / 333.87f + 21.f;
-
-    data.gyro.axis[1] = (int16_t)((buf[8] << 8) | buf[9]);
-    data.gyro.axis[0] = (int16_t)((buf[10] << 8) | buf[11]);
-    data.gyro.axis[2] = (int16_t)((buf[12] << 8) | buf[13]);
-    break;
-  }
-
-  case GYRO_TYPE_ICM42605:
-  case GYRO_TYPE_ICM42688P: {
-    uint8_t buf[14];
-    icm42605_read_data(ICM42605_TEMP_DATA1, buf, 14);
-
-    data.temp = (float)((int16_t)((buf[0] << 8) | buf[1])) / 132.48f + 25.f;
-
-    data.accel.axis[0] = -(int16_t)((buf[2] << 8) | buf[3]);
-    data.accel.axis[1] = -(int16_t)((buf[4] << 8) | buf[5]);
-    data.accel.axis[2] = (int16_t)((buf[6] << 8) | buf[7]);
-
-    data.gyro.axis[1] = (int16_t)((buf[8] << 8) | buf[9]);
-    data.gyro.axis[0] = (int16_t)((buf[10] << 8) | buf[11]);
-    data.gyro.axis[2] = (int16_t)((buf[12] << 8) | buf[13]);
-
-    break;
-  }
-
-  case GYRO_TYPE_BMI270: {
-    uint8_t buf[12];
-    bmi270_read_data(BMI270_REG_ACC_DATA_X_LSB, buf, 12);
-
-    data.accel.axis[0] = -(int16_t)((buf[1] << 8) | buf[0]);
-    data.accel.axis[1] = -(int16_t)((buf[3] << 8) | buf[2]);
-    data.accel.axis[2] = (int16_t)((buf[5] << 8) | buf[4]);
-
-    data.gyro.axis[1] = (int16_t)((buf[7] << 8) | buf[6]);
-    data.gyro.axis[0] = (int16_t)((buf[9] << 8) | buf[8]);
-    data.gyro.axis[2] = (int16_t)((buf[11] << 8) | buf[10]);
-
-    data.temp = 0;
-    break;
-  }
-
-=======
 
     data.accel.axis[0] = -(int16_t)((buf[0] << 8) + buf[1]);
     data.accel.axis[1] = -(int16_t)((buf[2] << 8) + buf[3]);
@@ -194,7 +141,6 @@ gyro_data_t gyro_spi_read() {
     break;
   }
 
->>>>>>> 53276eff (auto detect gyro type)
   default:
     break;
   }
